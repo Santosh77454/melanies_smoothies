8dd0431a3de07bfbe6011f539c8d04fa5b924f67
Santosh77454 <gayakesantosh994@gmail.com>
2025-09-16 20:10:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,6 +65,7 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
+
  
 
       
