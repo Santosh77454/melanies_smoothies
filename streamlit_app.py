a7a00e38120e86c2249c97294587d772befae3fe
Santosh77454 <gayakesantosh994@gmail.com>
2025-09-16 18:03:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
   #st.write(my_insert_stmt)
   time_to_insert = st.button('Submit Order')
     
-  if time_to_insert:
+if time_to_insert:
      session.sql(my_insert_stmt).collect()
       
      st.success('Your Smoothie is ordered!', icon="✅")
